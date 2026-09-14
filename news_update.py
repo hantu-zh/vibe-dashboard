508694cc1498c7a4742fa78787f188fd8fbd64fc
hantu-zh <chinasnm@126.com>
2026-09-14 21:23:44 +0800

    chore: 写入 news.html 时强制补齐 market_tab/kline 外部脚本引用及版本号 20260914e/20260914h

--- news_update.py
+++ news_update.py
@@ -890,13 +890,13 @@ def ensure_external_market_tab(html):
         inner = m.group(1)
         if len(inner) > 10000 and ('EM_TABS' in inner or 'loadMarketTab' in inner or 'INDEX_DEFS' in inner):
             start, end = m.start(), m.end()
-            html = html[:start] + '<script src="market_tab.js?v=20260914d"></script>' + html[end:]
+            html = html[:start] + '<script src="market_tab.js?v=20260914e"></script>' + html[end:]
             return html
     return html
 
 
-MARKET_TAB_REF = '<script src="market_tab.js?v=20260914d"></script>'
-KLINE_REF      = '<script src="kline_global_popup.js?v=20260914g"></script>'
+MARKET_TAB_REF = '<script src="market_tab.js?v=20260914e"></script>'
+KLINE_REF      = '<script src="kline_global_popup.js?v=20260914h"></script>'
 
 
 def ensure_bottom_scripts(html):
