9bfe123dca9227dc9a9f35837f6344ad1839d80a
hantu-zh <chinasnm@126.com>
2026-09-14 20:50:22 +0800

    fix: KLINE_REF 版本号同步 20260914g

--- news_update.py
+++ news_update.py
@@ -896,7 +896,7 @@ def ensure_external_market_tab(html):
 
 
 MARKET_TAB_REF = '<script src="market_tab.js?v=20260914d"></script>'
-KLINE_REF      = '<script src="kline_global_popup.js?v=20260914f"></script>'
+KLINE_REF      = '<script src="kline_global_popup.js?v=20260914g"></script>'
 
 
 def ensure_bottom_scripts(html):
