3d95e0dee0a0748d329f6f8b163b024e40d27215
hantu-zh <chinasnm@126.com>
2026-09-18 09:00:10 +0800

    chore(news): bump kline_global_popup.js ?v to 20260918a

--- news_update.py
+++ news_update.py
@@ -903,7 +903,7 @@ def ensure_external_market_tab(html):
 
 
 MARKET_TAB_REF = '<script src="market_tab.js?v=20260916c"></script>'
-KLINE_REF      = '<script src="kline_global_popup.js?v=20260915a"></script>'
+KLINE_REF      = '<script src="kline_global_popup.js?v=20260918a"></script>'
 
 
 def ensure_bottom_scripts(html):
